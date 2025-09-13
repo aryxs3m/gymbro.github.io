6d698f1c85fb61777c4cc12ed9166fb6c42fd8c9
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-09-13 20:10:26 +0200

    fixed missing mobile paddings

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ import '~/assets/style.css';
 
 <template>
   <div class="bg-red-500">
-    <div class="container mx-auto py-5">
+    <div class="container mx-auto py-5 px-5 md:px-0">
       This website is under construction. <strong>Repflux</strong> is under development. Broken links, missing or invalid
       content may occur.
     </div>
--- app/components/SimpleFeatureItem.vue
+++ app/components/SimpleFeatureItem.vue
@@ -4,7 +4,7 @@ defineProps(['heading'])
 
 <template>
   <div>
-    <div class="py-4 my-20 md:my-50">
+    <div class="py-4 my-20 md:my-50 px-10">
       <slot name="top"></slot>
       <h1 class="text-4xl mb-3">{{ heading }}</h1>
       <hr class="w-30 mx-auto my-5 border-0 border-b-3 border-b-primary">
