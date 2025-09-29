6692f19bf14b4d3cbe506408b292e26a2d5167d3
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2025-09-29 09:10:36 +0200

    RO-18: added default SEO tags RO-19: added favicon RO-20: added basic OpenGraph image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,22 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Repflux - open source workout tracker',
+      meta: [
+        { name: 'description', content: 'Keep track of your workouts, your body measurements and your weight. See your progression real time.' },
+        { name: 'ogImage', content: 'https://repflux.app/og.jpg' },
+      ],
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      ],
+    },
+  },
+
   runtimeConfig: {
     public: {
       demoUrl: 'https://demo.repflux.app',
